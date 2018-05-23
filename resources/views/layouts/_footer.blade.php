c978e7e684905518abef50ab1a3aca5aa9f791af
wenlin <wenlin42@icloud.com>
2018-05-23 06:55:42 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://github.com/IronnMan/larabbs" target="_blank">wenlin</a> 设计和编码 <span style="color:#e27575;font-size:14px;">❤️</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:huangwenlin42@gmail.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
